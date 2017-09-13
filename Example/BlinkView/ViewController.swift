84b8536ce74ffd8c35c94b9887bfa69a2e133483
Anuradh Caldera <anuradh@Anuradhs-MacBook-Pro.local>
2017-09-13 10:54:01 +0530

    Initial commit

---
+++ Example/BlinkView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BlinkView
+//
+//  Created by AnuradhSC on 09/13/2017.
+//  Copyright (c) 2017 AnuradhSC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
